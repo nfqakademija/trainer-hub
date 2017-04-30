6b437d19c1581d81da3ded70b59741048e0d703a
aurimas <auvanas@gmail.com>
2017-04-30 19:30:27 +0300

    code style update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Entity\Training;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Form\Type\FilterType;
 
 class DefaultController extends Controller
 {
@@ -15,14 +18,28 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
         $em = $this->getDoctrine()->getManager();
+        $trainersRepo = $em->getRepository(User::class);
+        $trainingsRepo = $em->getRepository(Training::class);
+        $cities = $trainingsRepo->findCities();
+        $categories = $trainingsRepo->findCategories();
+        foreach ($categories as $category) {
+            $categoriesNew[] = $category['title'];
+        }
+        foreach ($cities as $city) {
+            $citiesNew[] = $city['title'];
+        }
 
-        $trainers = $em->getRepository(User::class);
-
-        $trainers = $trainers->findByRoles('ROLE_TRAINER');
-
-        $paginator  = $this->get('knp_paginator');
+        if (!empty($_GET['categories']) && !empty($_GET['cities'])) {
+            $trainers = $trainersRepo->filterBoth($_GET['categories'], $_GET['cities']);
+        } elseif (empty($_GET['categories']) && !empty($_GET['cities'])) {
+            $trainers = $trainersRepo->filterByCity($_GET['cities']);
+        } elseif (!empty($_GET['categories']) && empty($_GET['cities'])) {
+            $trainers = $trainersRepo->filterByCategory($_GET['categories']);
+        } else {
+            $trainers = $trainersRepo->findByRoles('ROLE_TRAINER');
+        }
+        $paginator = $this->get('knp_paginator');
         $trainers = $paginator->paginate(
             $trainers, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
@@ -31,6 +48,10 @@ class DefaultController extends Controller
 
         return $this->render('@App/public/index.html.twig', [
             'trainers' => $trainers,
+            'cities' => array_unique($citiesNew),
+            'categories' => array_unique($categoriesNew),
+            'currentCity' => isset($_GET['cities'])?$_GET['cities']:'all',
+            'currentCategory' => isset($_GET['categories'])?$_GET['categories']:'all',
         ]);
     }
 }
